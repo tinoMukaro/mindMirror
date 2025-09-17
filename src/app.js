1ca8a224d48e14a29dcdeaf9d6e1fc052536fa1c
tinoMukaro <vamukaro3@gmail.com>
2025-09-17 23:18:02 +0200

    set up logger and middleware

--- src/app.js
+++ src/app.js
@@ -1,7 +1,22 @@
 import express from 'express';
+import logger from './config/logger.js';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+
 const app = express();
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('combined', { stream: { write: message => logger.info(message.trim()) } }));
+app.use(cookieParser());
+
 
 app.get('/', (req, res) => {
+  logger.info('Root endpoint accessed');
   res.status(200).send('Hello, there!');
 });
 
