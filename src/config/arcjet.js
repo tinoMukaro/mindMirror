93dd8002c189d3dcfb7730b6a69362adb35c1e30
tinoMukaro <vamukaro3@gmail.com>
2025-09-18 13:19:43 +0200

    secure my api with arcjet

---
+++ src/config/arcjet.js
@@ -0,0 +1,20 @@
+import arcjet, { shield, detectBot, slidingWindow } from '@arcjet/node';
+import 'dotenv/config';
+
+const aj = arcjet({
+  key: process.env.ARCJET_KEY,
+  rules: [
+    shield({ mode: 'LIVE' }),
+    detectBot({
+      mode: 'LIVE',
+      allow: ['CATEGORY:SEARCH_ENGINE', 'CATEGORY:PREVIEW'],
+    }),
+    slidingWindow({
+      mode: 'LIVE',
+      interval: '2s',
+      max: 5,
+    }),
+  ],
+});
+
+export default aj;
