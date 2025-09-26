6b0d6562874c0c821e4c9401e26d6cfef8b35c59
tinoMukaro <vamukaro3@gmail.com>
2025-09-26 23:58:09 +0200

    fix logger

--- backend/src/config/logger.js
+++ backend/src/config/logger.js
@@ -1,28 +1,34 @@
 import winston from 'winston';
 
+const transports = [];
+
+// Console logging is always good
+transports.push(
+  new winston.transports.Console({
+    format: winston.format.combine(
+      winston.format.colorize(),
+      winston.format.simple()
+    )
+  })
+);
+
+// Only log to files in local/dev environment
+if (process.env.NODE_ENV !== 'production') {
+  transports.push(
+    new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'logs/combined.log' })
+  );
+}
+
 const logger = winston.createLogger({
   level: 'info',
   format: winston.format.combine(
-    (winston.format.timestamp(),
+    winston.format.timestamp(),
     winston.format.errors({ stack: true }),
-    winston.format.json())
+    winston.format.json()
   ),
   defaultMeta: { service: 'mindMirror' },
-  transports: [
-    new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'logs/combined.log' }),
-  ],
+  transports
 });
-
-if (process.env.NODE_ENV !== 'production') {
-  logger.add(
-    new winston.transports.Console({
-      format: winston.format.combine(
-        winston.format.colorize(),
-        winston.format.simple()
-      ),
-    })
-  );
-}
 
 export default logger;
