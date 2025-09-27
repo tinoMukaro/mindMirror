88523e605693065d10cb4f251e564130f7bfd92e
tinoMukaro <vamukaro3@gmail.com>
2025-09-27 21:33:42 +0200

    fix cookie settings

--- backend/src/utils/cookies.js
+++ backend/src/utils/cookies.js
@@ -2,7 +2,7 @@ export const cookies = {
   getOptions: () => ({
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict',
+    sameSite: 'none',
     maxAge: 15 * 60 * 1000,
   }),
 
