c8537ce634ed5eb5e555b2607eb93f711c9bcdf7
tino Mukaro <vamukaro3@gmail.com>
2026-01-05 13:27:42 +0200

    Change Analytics import from next to react

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import Journal from "./pages/Journal";
 import Footer from "./components/Footer";
 import Insights from "./pages/Insights";
 import AuthPage from "./pages/AuthPage";
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 
 
 function App() {
